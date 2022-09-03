cd9c9790c948b065ed8094c6d1921a71ec280f65
tanvir <tanvir101tk@gmail.com>
2022-09-04 02:14:39 +0600

    modal part complete

--- news.js
+++ news.js
@@ -46,7 +46,7 @@ const displayNews = newsAll => {
         const div = document.createElement('div');
         div.classList.add('row')
         div.innerHTML = `
-        <div class="col-lg-4 col-sm-12 my-3"><img src="${thumbnail_url == null ? 'no data available' : thumbnail_url}" alt=""></div>
+        <div class="col-lg-4 col-sm-12 my-3 img-fluid"><img src="${thumbnail_url == null ? 'no data available' : thumbnail_url}" alt=""></div>
         <div class="col-lg-8 col-sm-12 my-3">
             <h3>${title}</h3>
             <p>${details.length > 400 ? details.slice(0, 400) + '...' : details}</p>
@@ -56,11 +56,12 @@ const displayNews = newsAll => {
                     <h6>${author.name == null ? 'no data available' : author.name}</h6>
                 </div>
                 <div><i class="fa fa-eye" aria-hidden="true"></i> ${total_view == null ? 'no data available' : total_view}</div>
-                <button class="btn btn-primary">view</button>
+                <button onclick="modalDetail('${news._id}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">View</button>
             </div>
         </div>
         `;
         newsId.appendChild(div);
+
     });
 }
 
@@ -75,6 +76,35 @@ const toggleSpinner = isSpinning => {
         spinnerSection.classList.add('d-none')
     }
 }
+
+// modal details
+
+
+const modalDetail = id => {
+    fetch(`https://openapi.programming-hero.com/api/news/${id}`)
+        .then(res => res.json())
+        .then(data => displayDetail(data.data[0]))
+}
+
+const displayDetail = detail => {
+    const { author, details, image_url, title, total_view } = detail;
+    const findDiv = document.getElementById('modal-body');
+    findDiv.innerHTML = '';
+    const createDiv = document.createElement('div');
+    createDiv.innerHTML = `
+    <img class="w-100 img-fluid" src="${image_url == null ? 'no data available' : image_url}" alt="">
+    <h5>${title}</h5>
+    <p>${details.length > 400 ? details.slice(0, 400) + '...' : details}</p>
+       <div class="image-div">
+           <img src="${author.img == null ? 'no data available' : author.img}" alt=""></div>
+           <h6>${author.name == null ? 'no data available' : author.name}</h6>
+       </div>
+       <div><i class="fa fa-eye" aria-hidden="true"></i> ${total_view == null ? 'no data available' : total_view}</div>
+
+        `;
+    findDiv.appendChild(createDiv);
+}
+
 
 
 
